f620f8bd9a73cda4ddb886f740b39fe6a8d48e3d
Byron Wilson <bywil5@morgan.edu>
2020-02-19 18:08:04 -0500

    took out "!"

--- FlixApp/MoviesViewController.swift
+++ FlixApp/MoviesViewController.swift
@@ -27,7 +27,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
         
-        cell.posterView.af_setImage(withURL: posterUrl!)
+        cell.posterView.af_setImage(withURL: posterUrl)
         return cell
     }
     
