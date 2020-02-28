f18e628a7cc954864be232093140e2f54b1f80a9
Byron Wilson <ronwilson402@gmail.com>
2020-02-28 18:35:48 -0500

    Added Collection View Controller(with errors)

---
+++ FlixApp/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  FlixApp
+//
+//  Created by Byron M Wilson on 2/28/20.
+//  Copyright © 2020 Byron M Wilson. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
